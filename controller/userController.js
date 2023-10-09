96103b54bd27aa50c27a706dcfaae10e3650bb33
Anguh Bleaise <bleaise.anguh.quigo@gmail.com>
2023-10-09 05:02:21 +0100

    handling user errors

--- controller/userController.js
+++ controller/userController.js
@@ -1,17 +1,17 @@
 const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
-const createUser = async (req, res) => {
-    const email = req.body.email;
-    const findUser = await  User.findOne({email: email});
-    if (!findUser){
-        const newUser = await User.create(req.body)
-        res.json(newUser);
-    }else {
-        res.json({
-            message: 'User already exist',
-            success: false
-        });
+const createUser = asyncHandler(
+    async (req, res) => {
+        const email = req.body.email;
+        const findUser = await  User.findOne({email: email});
+        if (!findUser){
+            const newUser = await User.create(req.body)
+            res.json(newUser);
+        }else {
+           throw new Error('User already exist');
+        }
     }
-}
+);
 
 module.exports={ createUser }
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require('./routes/authRoute');
 const bodyParser = require("body-parser");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 dbConnect();
 app.use(bodyParser.json());
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/user', authRouter);
 
 
+
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 });
